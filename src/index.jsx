2be0d098d2cb1c15bf8c106810f4672ee49e3248
Justin Graber <jgraberdev@gmail.com>
2016-06-29 10:50:11 -0500

    Add React-Redux + test specs to app

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {List, Map} from 'immutable';
-
-import TodoApp from './components/TodoApp'
+import {createStore} from 'redux';
+import {Provider} from './react-redux';
+import reducer from './reducer';
+import {TodoAppContainer} from './components/TodoApp'
 
 const todos = List.of(
   Map({id: 1, text: 'React', status: 'active', editing: false}),
@@ -10,9 +12,25 @@ const todos = List.of(
   Map({id: 3, text: 'Immutable', status: 'completed', editing: false})
 );
 
-let filter = 'all';
+// Instantiate new Redux Store
+const store = createStore(reducer);
+
+// Set intial state
+store.dispatch({
+  type: 'SET_STATE',
+  state: {
+    todos: [
+      {id: 1, text: 'React', status: 'active', editing: false},
+      {id: 2, text: 'Redux', status: 'active', editing: false},
+      {id: 3, text: 'Immutable', status: 'active', editing: false},
+    ],
+    filter: 'all'
+  }
+});
 
 ReactDOM.render(
-  <TodoApp filter={filter} todos={todos}></TodoApp>,
+  <Provider store={store}>
+      <TodoAppContainer></TodoAppContainer>
+  </Provider>
   document.getElementById('app')
 );
