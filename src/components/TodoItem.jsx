01406435305d59d2b47a60b9e1aa3e9b72eefaad
Justin Graber <jgraberdev@gmail.com>
2016-06-29 08:58:44 -0500

    Spec and continued functionality for TodoItem

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,23 +1,28 @@
 import React from 'react';
 import TextInput from './TextInput';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
+import classNames from 'classnames';
 
 export default class TodoItem extends React.Component {
     constructor(props) {
         super(props);
         this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+        this.deleteItem = () => this.props.deleteItem(this.props.id);
     }
     render() {
-      return <li className="todo">
-        <div className="view">
-          <input type="checkbox"
-                 className="toggle" />
-          <label htmlFor="todo">
-            {this.props.text}
-          </label>
-          <button className="destroy"></button>
-        </div>
-        <TextInput></TextInput>
-      </li>
+        var itemClass = classNames({
+          'todo': true,
+          'completed': this.props.isCompleted,
+          'editing': this.props.isEditing
+        });
+
+        return <li className={itemClass}>
+          <div className="view">
+            <input type="checkbox" className="toggle" defaultChecked={this.props.isCompleted}></input>
+            <label htmlFor="todo">{this.props.text}</label>
+            <button className="destroy" onClick={this.deleteItem}></button>
+          </div>
+          <TextInput></TextInput>
+        </li>
     }
 };
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,6 +3,10 @@ import TodoItem from './TodoItem';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 
 export default class TodoList extends React.Component {
+    // This function checks whether an item is completed
+    isCompleted(item) {
+        return item.get('status') === 'completed';
+    }
     getItems() {
         if (this.props.todos) {
             return this.props.todos.filter(todo => this.props.filter === 'all' || todo.get('status') === this.props.filter);
@@ -17,8 +21,12 @@ export default class TodoList extends React.Component {
       return <div className="main">
         <ul className="todo-list">
             {this.getItems().map(todo =>
-                <TodoItem key={todo.get('text')}
-                          text={todo.get('text')} />
+                <TodoItem
+                  key={todo.get('text')}
+                  text={todo.get('text')}
+                  isCompleted={this.isCompleted(todo)}
+                  isEditing={todo.get('editing')}
+                ></TodoItem>
             )}
         </ul>
       </div>
