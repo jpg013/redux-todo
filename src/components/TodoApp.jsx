2a4f869b3ef591d1d3fe3023a6e4f6c9f82680cd
Justin Graber <jgraberdev@gmail.com>
2016-06-30 08:41:32 -0500

    Action Creators. Handle Change Filter Spec

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import TodoList from './TodoList';
 import {connect} from 'react-redux';
+import * as actionCreators from '../action_creators';
 
 require("../../styles/app.css");
 
 export class TodoApp extends React.Component {
   render() {
     return <div>
-        <TodoList filter={this.props.filter} todos={this.props.todos}></TodoList>
+        <TodoList {...this.props}></TodoList>
     </div>
   }
 };
@@ -19,4 +20,4 @@ function mapStateToProps(state) {
     };
 }
 
-export const TodoAppContainer = connect(mapStateToProps)(TodoApp);
+export const TodoAppContainer = connect(mapStateToProps, actionCreators)(TodoApp);
